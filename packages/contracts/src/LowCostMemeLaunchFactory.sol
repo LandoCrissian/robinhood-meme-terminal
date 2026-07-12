f8ade4296aa23de899ea9654b72ccba466db59ff
N0IR2KX <landon361@gmail.com>
2026-07-12 17:32:46 -0600

    Refactor low-cost launch core for V2

--- packages/contracts/src/LowCostMemeLaunchFactory.sol
+++ packages/contracts/src/LowCostMemeLaunchFactory.sol
@@ -83,6 +83,17 @@ contract LowCostMemeLaunchFactory {
         address[4] calldata communityRecipients,
         uint16[5] calldata rewardBps
     ) external returns (address token, address market, address rewardVault) {
+        return _launch(msg.sender, name, symbol, metadataURI, communityRecipients, rewardBps);
+    }
+
+    function _launch(
+        address creator,
+        string calldata name,
+        string calldata symbol,
+        string calldata metadataURI,
+        address[4] memory communityRecipients,
+        uint16[5] memory rewardBps
+    ) internal returns (address token, address market, address rewardVault) {
         if (bytes(name).length == 0) revert EmptyName();
         if (bytes(symbol).length == 0) revert EmptySymbol();
 
@@ -92,20 +103,19 @@ contract LowCostMemeLaunchFactory {
         if (_usedSymbolHashes[symbolHash]) revert DuplicateSymbol();
 
         uint256 totalBps;
-        for (uint256 i; i < rewardBps.length; ++i) totalBps += rewardBps[i];
+        for (uint256 i; i < rewardBps.length; ++i) {
+            totalBps += rewardBps[i];
+        }
         if (totalBps != 10_000) revert InvalidRewardSplit();
 
-        address[5] memory recipients = [
-            msg.sender, communityRecipients[0], communityRecipients[1], communityRecipients[2], communityRecipients[3]
-        ];
+        address[5] memory recipients =
+            [creator, communityRecipients[0], communityRecipients[1], communityRecipients[2], communityRecipients[3]];
 
         _usedNameHashes[nameHash] = true;
         _usedSymbolHashes[symbolHash] = true;
 
         token = MinimalProxy.clone(tokenImplementation);
-        CloneFixedSupplyMemeToken(token).initialize(
-            name, symbol, TOKEN_SUPPLY, msg.sender, address(this), metadataURI
-        );
+        CloneFixedSupplyMemeToken(token).initialize(name, symbol, TOKEN_SUPPLY, creator, address(this), metadataURI);
 
         bytes32 poolId = IGraduationAdapter(graduationAdapter).prepare(token);
         if (poolId == bytes32(0)) revert InvalidPoolReservation();
@@ -114,25 +124,24 @@ contract LowCostMemeLaunchFactory {
         CloneLaunchRewardVault(payable(rewardVault)).initialize(recipients, rewardBps);
 
         market = MinimalProxy.clone(marketImplementation);
-        CloneBondingCurveMarket(payable(market)).initialize(
-            token,
-            payable(rewardVault),
-            graduationAdapter,
-            poolId,
-            marketFeeBps,
-            initialVirtualEthReserve,
-            initialVirtualTokenReserve,
-            graduationTarget
-        );
+        CloneBondingCurveMarket(payable(market))
+            .initialize(
+                token,
+                payable(rewardVault),
+                graduationAdapter,
+                poolId,
+                marketFeeBps,
+                initialVirtualEthReserve,
+                initialVirtualTokenReserve,
+                graduationTarget
+            );
 
         IGraduationAdapter(graduationAdapter).bindMarket(token, market);
         if (!CloneFixedSupplyMemeToken(token).transfer(market, TOKEN_SUPPLY)) revert InventoryTransferFailed();
 
         uint256 launchId = _launches.length;
-        _launches.push(Launch(token, market, rewardVault, poolId, msg.sender, uint64(block.timestamp)));
-        emit TokenLaunched(
-            launchId, token, msg.sender, market, rewardVault, poolId, name, symbol, metadataURI, rewardBps
-        );
+        _launches.push(Launch(token, market, rewardVault, poolId, creator, uint64(block.timestamp)));
+        emit TokenLaunched(launchId, token, creator, market, rewardVault, poolId, name, symbol, metadataURI, rewardBps);
     }
 
     function launchCount() external view returns (uint256) {
