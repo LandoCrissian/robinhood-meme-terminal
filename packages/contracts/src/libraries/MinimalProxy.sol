f3bc74a94d0c00d492b85936299618e178567774
N0IR2KX <landon361@gmail.com>
2026-07-12 14:25:30 -0600

    Fix EIP-1167 clone deployment bytecode

--- packages/contracts/src/libraries/MinimalProxy.sol
+++ packages/contracts/src/libraries/MinimalProxy.sol
@@ -5,11 +5,16 @@ library MinimalProxy {
     error CloneDeploymentFailed();
 
     function clone(address implementation) internal returns (address instance) {
+        bytes memory creationCode = abi.encodePacked(
+            hex"3d602d80600a3d3981f3",
+            hex"363d3d373d3d3d363d73",
+            implementation,
+            hex"5af43d82803e903d91602b57fd5bf3"
+        );
+
         assembly ("memory-safe") {
-            mstore(0x00, or(shr(0xe8, shl(0x60, implementation)), 0x3d602d80600a3d3981f3))
-            mstore(0x20, or(shl(0x78, implementation), 0x5af43d82803e903d91602b57fd5bf3))
-            instance := create(0, 0x09, 0x37)
+            instance := create(0, add(creationCode, 0x20), mload(creationCode))
         }
-        if (instance == address(0)) revert CloneDeploymentFailed();
+        if (instance == address(0) || instance.code.length == 0) revert CloneDeploymentFailed();
     }
 }
