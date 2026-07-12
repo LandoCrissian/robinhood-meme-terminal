9fbb73f09b2dfda603404a7dded58d944c4271ae
Landon Little <landon@Landons-MacBook-Pro.local>
2026-07-11 21:53:04 -0600

    Factory: separate immutable testnet economics

--- packages/contracts/script/DeployMemeLaunchFactory.s.sol
+++ packages/contracts/script/DeployMemeLaunchFactory.s.sol
@@ -13,6 +13,10 @@ interface Vm {
 contract DeployMemeLaunchFactory {
     Vm private constant vm = Vm(address(uint160(uint256(keccak256("hevm cheat code")))));
     uint256 private constant ROBINHOOD_TESTNET_CHAIN_ID = 46_630;
+    uint16 private constant TESTNET_MARKET_FEE_BPS = 100;
+    uint256 private constant TESTNET_VIRTUAL_ETH_RESERVE = 0.01 ether;
+    uint256 private constant TESTNET_VIRTUAL_TOKEN_RESERVE = 1_073_000_000 ether;
+    uint256 private constant TESTNET_GRADUATION_TARGET = 0.001 ether;
 
     error WrongChain(uint256 actualChainId);
 
@@ -22,7 +26,13 @@ contract DeployMemeLaunchFactory {
         uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
         address graduationAdapter = vm.envAddress("GRADUATION_ADAPTER");
         vm.startBroadcast(deployerPrivateKey);
-        factory = new MemeLaunchFactory(graduationAdapter);
+        factory = new MemeLaunchFactory(
+            graduationAdapter,
+            TESTNET_MARKET_FEE_BPS,
+            TESTNET_VIRTUAL_ETH_RESERVE,
+            TESTNET_VIRTUAL_TOKEN_RESERVE,
+            TESTNET_GRADUATION_TARGET
+        );
         vm.stopBroadcast();
     }
 }
