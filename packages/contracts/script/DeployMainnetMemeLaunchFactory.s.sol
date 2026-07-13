337e16f88e504b208b48d9b4b4b2c071684a93ba
N0IR2KX <landon361@gmail.com>
2026-07-13 09:25:00 -0600

    mainnet: auto-deploy governance and purpose vaults

--- packages/contracts/script/DeployMainnetMemeLaunchFactory.s.sol
+++ packages/contracts/script/DeployMainnetMemeLaunchFactory.s.sol
@@ -3,8 +3,10 @@ pragma solidity ^0.8.26;
 
 import {CloneBondingCurveMarketV2} from "../src/clone/CloneBondingCurveMarketV2.sol";
 import {LowCostMemeLaunchFactoryV4} from "../src/LowCostMemeLaunchFactoryV4.sol";
+import {ProtocolPurposeVault} from "../src/ProtocolPurposeVault.sol";
 import {ProtocolRevenueRouter} from "../src/ProtocolRevenueRouter.sol";
 import {PurposeRewardsController} from "../src/PurposeRewardsController.sol";
+import {TwoOfThreeTimelock} from "../src/TwoOfThreeTimelock.sol";
 import {VersionedFactoryRegistry} from "../src/VersionedFactoryRegistry.sol";
 import {V4GraduationAdapter} from "../src/V4GraduationAdapter.sol";
 import {V4GraduationHook} from "../src/V4GraduationHook.sol";
@@ -24,47 +26,34 @@ interface MainnetVm {
 contract DeployMainnetMemeLaunchFactory {
     MainnetVm private constant vm = MainnetVm(address(uint160(uint256(keccak256("hevm cheat code")))));
 
+    bytes32 private constant TREASURY_PURPOSE = keccak256("PROTOCOL_TREASURY");
+    bytes32 private constant BUYBACK_PURPOSE = keccak256("BUYBACK_RESERVE");
+    bytes32 private constant GRADUATION_PURPOSE = keccak256("GRADUATION_ASSISTANCE");
+    bytes32 private constant REFERRAL_PURPOSE = keccak256("REFERRAL_RESERVE");
+    bytes32 private constant ECOSYSTEM_PURPOSE = keccak256("ECOSYSTEM_GROWTH");
+
     error WrongChain(uint256 actualChainId);
     error MissingCanonicalContract(address account);
     error InvalidOperatorAddress();
-    error DuplicateRevenueRecipient();
+    error DuplicateSigner();
     error HookAddressMismatch();
     error BindingVerificationFailed();
 
-    function run()
-        external
-        returns (
-            V4GraduationHook hook,
-            V4GraduationAdapter adapter,
-            ProtocolRevenueRouter revenueRouter,
-            PurposeRewardsController rewardsController,
-            LowCostMemeLaunchFactoryV4 factory,
-            VersionedFactoryRegistry registry
-        )
-    {
+    function run() external {
         if (block.chainid != Config.CHAIN_ID) revert WrongChain(block.chainid);
         if (Config.POOL_MANAGER.code.length == 0) revert MissingCanonicalContract(Config.POOL_MANAGER);
         if (Config.CREATE2_DEPLOYER.code.length == 0) revert MissingCanonicalContract(Config.CREATE2_DEPLOYER);
 
         uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
         address deployer = vm.addr(deployerPrivateKey);
-        address rewardsGovernance = vm.envAddress("REWARDS_GOVERNANCE");
-        address governance = vm.envAddress("FACTORY_GOVERNANCE");
-        address[5] memory revenueRecipients = [
-            vm.envAddress("TREASURY_RECIPIENT"),
-            vm.envAddress("BUYBACK_RESERVE_RECIPIENT"),
-            vm.envAddress("GRADUATION_ASSISTANCE_RECIPIENT"),
-            vm.envAddress("REFERRAL_RESERVE_RECIPIENT"),
-            vm.envAddress("ECOSYSTEM_GROWTH_RECIPIENT")
-        ];
+        address[3] memory signers =
+            [vm.envAddress("SIGNER_ONE"), vm.envAddress("SIGNER_TWO"), vm.envAddress("SIGNER_THREE")];
 
-        if (deployer == address(0) || rewardsGovernance == address(0) || governance == address(0)) {
-            revert InvalidOperatorAddress();
-        }
-        for (uint256 i; i < revenueRecipients.length; ++i) {
-            if (revenueRecipients[i] == address(0)) revert InvalidOperatorAddress();
+        if (deployer == address(0)) revert InvalidOperatorAddress();
+        for (uint256 i; i < signers.length; ++i) {
+            if (signers[i] == address(0)) revert InvalidOperatorAddress();
             for (uint256 j; j < i; ++j) {
-                if (revenueRecipients[i] == revenueRecipients[j]) revert DuplicateRevenueRecipient();
+                if (signers[i] == signers[j]) revert DuplicateSigner();
             }
         }
 
@@ -74,17 +63,34 @@ contract DeployMainnetMemeLaunchFactory {
             HookMiner.find(Config.CREATE2_DEPLOYER, flags, type(V4GraduationHook).creationCode, constructorArgs);
 
         vm.startBroadcast(deployerPrivateKey);
-        hook = new V4GraduationHook{salt: salt}(IPoolManager(Config.POOL_MANAGER), deployer);
+
+        TwoOfThreeTimelock factoryGovernance = new TwoOfThreeTimelock(signers, 0);
+        TwoOfThreeTimelock rewardsGovernance = new TwoOfThreeTimelock(signers, 0);
+        TwoOfThreeTimelock protocolGovernance =
+            new TwoOfThreeTimelock(signers, Config.PROTOCOL_GOVERNANCE_DELAY);
+
+        address[5] memory revenueRecipients = [
+            address(new ProtocolPurposeVault(address(protocolGovernance), TREASURY_PURPOSE)),
+            address(new ProtocolPurposeVault(address(protocolGovernance), BUYBACK_PURPOSE)),
+            address(new ProtocolPurposeVault(address(protocolGovernance), GRADUATION_PURPOSE)),
+            address(new ProtocolPurposeVault(address(protocolGovernance), REFERRAL_PURPOSE)),
+            address(new ProtocolPurposeVault(address(protocolGovernance), ECOSYSTEM_PURPOSE))
+        ];
+
+        V4GraduationHook hook =
+            new V4GraduationHook{salt: salt}(IPoolManager(Config.POOL_MANAGER), deployer);
         if (address(hook) != expectedHook) revert HookAddressMismatch();
 
-        adapter = new V4GraduationAdapter(
+        V4GraduationAdapter adapter = new V4GraduationAdapter(
             IPoolManager(Config.POOL_MANAGER), hook, Config.V4_POOL_FEE, Config.V4_TICK_SPACING
         );
         hook.bindAdapter(address(adapter));
 
-        revenueRouter = new ProtocolRevenueRouter(revenueRecipients);
-        rewardsController = new PurposeRewardsController(deployer, rewardsGovernance, Config.REWARD_RELEASE_DELAY);
-        factory = new LowCostMemeLaunchFactoryV4(
+        ProtocolRevenueRouter revenueRouter = new ProtocolRevenueRouter(revenueRecipients);
+        PurposeRewardsController rewardsController = new PurposeRewardsController(
+            deployer, address(rewardsGovernance), Config.REWARD_RELEASE_DELAY
+        );
+        LowCostMemeLaunchFactoryV4 factory = new LowCostMemeLaunchFactoryV4(
             address(adapter),
             Config.MARKET_FEE_BPS,
             Config.INITIAL_VIRTUAL_ETH_RESERVE,
@@ -96,8 +102,8 @@ contract DeployMainnetMemeLaunchFactory {
         adapter.bindFactory(address(factory));
         rewardsController.bindFactory(address(factory));
 
-        registry = new VersionedFactoryRegistry(
-            governance, Config.FACTORY_ACTIVATION_DELAY, address(factory), Config.FACTORY_VERSION
+        VersionedFactoryRegistry registry = new VersionedFactoryRegistry(
+            address(factoryGovernance), Config.FACTORY_ACTIVATION_DELAY, address(factory), Config.FACTORY_VERSION
         );
         vm.stopBroadcast();
 
@@ -111,22 +117,37 @@ contract DeployMainnetMemeLaunchFactory {
                 || factory.platformTreasury() != address(revenueRouter)
                 || factory.rewardsController() != address(rewardsController)
                 || rewardsController.factory() != address(factory)
-                || rewardsController.governance() != rewardsGovernance
+                || rewardsController.governance() != address(rewardsGovernance)
                 || rewardsController.releaseDelay() != Config.REWARD_RELEASE_DELAY
                 || factory.marketFeeBps() != Config.MARKET_FEE_BPS
                 || factory.initialVirtualEthReserve() != Config.INITIAL_VIRTUAL_ETH_RESERVE
                 || factory.initialVirtualTokenReserve() != Config.INITIAL_VIRTUAL_TOKEN_RESERVE
-                || factory.graduationTarget() != Config.GRADUATION_TARGET || registry.governance() != governance
+                || factory.graduationTarget() != Config.GRADUATION_TARGET
+                || registry.governance() != address(factoryGovernance)
                 || registry.activationDelay() != Config.FACTORY_ACTIVATION_DELAY
                 || registry.activeFactory() != address(factory) || registry.activeVersion() != Config.FACTORY_VERSION
+                || factoryGovernance.executionDelay() != 0 || rewardsGovernance.executionDelay() != 0
+                || protocolGovernance.executionDelay() != Config.PROTOCOL_GOVERNANCE_DELAY
                 || marketImplementation.FAIR_START_DELAY_BLOCKS() != 3
                 || marketImplementation.FAIR_START_DURATION_BLOCKS() != 25
                 || marketImplementation.FAIR_START_MAX_TX_BPS() != 50
                 || marketImplementation.FAIR_START_MAX_WALLET_BPS() != 150
         ) revert BindingVerificationFailed();
 
+        bytes32[5] memory purposes =
+            [TREASURY_PURPOSE, BUYBACK_PURPOSE, GRADUATION_PURPOSE, REFERRAL_PURPOSE, ECOSYSTEM_PURPOSE];
+        for (uint256 i; i < signers.length; ++i) {
+            if (
+                factoryGovernance.signers(i) != signers[i] || rewardsGovernance.signers(i) != signers[i]
+                    || protocolGovernance.signers(i) != signers[i]
+            ) revert BindingVerificationFailed();
+        }
         for (uint256 i; i < revenueRecipients.length; ++i) {
-            if (revenueRouter.recipients(i) != revenueRecipients[i]) revert BindingVerificationFailed();
+            ProtocolPurposeVault vault = ProtocolPurposeVault(payable(revenueRecipients[i]));
+            if (
+                revenueRouter.recipients(i) != revenueRecipients[i]
+                    || vault.governance() != address(protocolGovernance) || vault.purpose() != purposes[i]
+            ) revert BindingVerificationFailed();
         }
     }
 }
